4c885864d2e7078c34cefc77a44c63661ff107c0
812jay <yongje812@gmail.com>
2021-07-10 05:41:44 +0900

    운동계획하기 카테고리별 운동조회 기능 추가

--- lib/ui/widget/my_calendar/workout_plan.dart
+++ lib/ui/widget/my_calendar/workout_plan.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:do_it_app/utils/define.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sticky_header/flutter_sticky_header.dart';
@@ -27,8 +29,17 @@ class _WorkoutPlanState extends State<WorkoutPlan> {
     '역도',
     '복근',
     '전신',
+    '유산소',
     '기타'
   ]; // 카테고리 리스트
+
+  List<Map> workouts = [
+    {'title': '스내치', 'category': '역도', 'bookmarked': true},
+    {'title': '풀업', 'category': '등', 'bookmarked': false},
+    {'title': '프론트 스쿼트', 'category': '하체', 'bookmarked': true},
+    {'title': '인클라인 벤치프레스', 'category': '가슴', 'bookmarked': true},
+    {'title': '인클라인 덤벨프레스', 'category': '가슴', 'bookmarked': false},
+  ];
 
   List<String> filterOptions = [];
 
@@ -113,9 +124,24 @@ class _WorkoutPlanState extends State<WorkoutPlan> {
                 ),
               ), //서브헤더
               sliver: SliverList(
-                delegate: SliverChildBuilderDelegate((context, index) {
-                  return getWorkouts();
-                }, childCount: 50), //운동목록
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    String selectedCategory = filterOptions[0];
+                    List<Widget> containWorkouts = [];
+                    for (int i = 0; i < workouts.length; i++) {
+                      String workoutsCategory = workouts[i]['category'];
+                      if (selectedCategory == workoutsCategory ||
+                          selectedCategory == '전체') {
+                        containWorkouts.add(getWorkouts(category: workouts[i]));
+                      }
+                    }
+                    return Column(
+                      children:
+                          containWorkouts.map((workout) => workout).toList(),
+                    );
+                  },
+                  childCount: 1,
+                ),
               ),
             ),
           ],
@@ -156,10 +182,46 @@ class _WorkoutPlanState extends State<WorkoutPlan> {
     );
   } //카테고리 버튼
 
-  Widget getWorkouts() {
-    return Container(
-      height: height * 0.1,
-      child: Text('data'),
+  Widget getWorkouts({required category}) {
+    return GestureDetector(
+      onTap: () {
+        print('button');
+      },
+      child: Container(
+        padding: EdgeInsets.only(
+          top: height * 0.01,
+          bottom: height * 0.01,
+          left: width * 0.05,
+          right: width * 0.05,
+        ),
+        margin: EdgeInsets.only(
+          bottom: height * 0.02,
+          left: width * 0.05,
+          right: width * 0.05,
+        ),
+        decoration: BoxDecoration(
+          border: Border.all(width: 3, color: Colors.black),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              '${category['category']} | ${category['title']}',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+            ),
+            GestureDetector(
+              onTap: () {
+                print('bookmark');
+              },
+              child: Icon(
+                Icons.bookmark_border,
+                size: 36,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
